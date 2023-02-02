065184921178a51108bc9efe46f8d04c29f8e3ab
Tiffany Taylor <tiffany@php.net>
2023-02-02 10:46:45 -0700

    Add value object for chat message metadata

--- src/Entities/MessageMetadata.php
+++ src/Entities/MessageMetadata.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace ChatScraper\Repository;
+namespace ChatScraper\Entities;
 
 readonly class MessageMetadata
 {
     public function __construct(
         public int $messageId,
         public string $content,
-        public int $amountOfEdits,
-        public int $parentId,
+        public ?int $amountOfEdits,
+        public ?int $parentId,
         public bool $isReply,
         public bool $isStarred,
-        public int $amountOfStars,
+        public ?int $amountOfStars,
         public bool $isPinnedByRoomOwner,
+        public ?int $amountOfStarsAndPins,
         public bool $hasBeenMoved,
     ) {
     }
