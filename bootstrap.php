b1ba7b93993f319c5aad50e5a5916bac8a8ff5c9
Tiffany Taylor <tiffany@php.net>
2023-02-02 10:49:18 -0700

    Testing out changes in bootstrap...

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $guzzle = new Client();
 
 $scanner = new Message($guzzle);
-$messages = $scanner->checkForMessages();
+$messages = $scanner->checkForMessages(2);
 
+$parser = new ChatScraper\Parser\Message($messages);
 
-var_dump($messages);
+$parsedMessages = $parser->parse();
+
+var_dump($parsedMessages);
