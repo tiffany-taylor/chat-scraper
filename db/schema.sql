02af181357c736aedfc8e1658507f76fb84c5fa9
Tiffany Taylor <tiffany@php.net>
2023-02-13 14:28:31 +0000

    Add docker files for DB storage
    Intent is to be able to save messages *somewhere* and using MySQL for now.
    Doesn't contain code for message storage yet, only setting up the docker instance.

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE messages (
+      messageId INT NOT NULL,
+      content MEDIUMTEXT NOT NULL,
+      amountOfEdits INT DEFAULT NULL,
+      parentId INT DEFAULT NULL,
+      isReply BOOL DEFAULT FALSE,
+      isStarred BOOL DEFAULT FALSE,
+      amountOfStars INT DEFAULT NULL,
+      isPinnedByRoomOwner BOOL DEFAULT FALSE,
+      amountOfStarsAndPins INT DEFAULT NULL,
+      hasBeenMoved BOOL DEFAULT FALSE,
+      userId INT NOT NULL,
+      PRIMARY KEY (messageId),
+      FOREIGN KEY (userId) REFERENCES users(userId)
+);
+
+CREATE TABLE users (
+       userId INT NOT NULL,
+       username VARCHAR(40) NOT NULL,
+       PRIMARY KEY (userId)
+);
